558feef29f39b54643e0ac4615898a749a4a9537
Mainuddin Rizvi <web97development@gmail.com>
2024-12-13 00:03:31 +0530

    [ADD] : Form Validation

--- helloworld/src/main/java/com/strutsguide/hello/action/Register.java
+++ helloworld/src/main/java/com/strutsguide/hello/action/Register.java
@@ -14,8 +14,21 @@ public class Register extends ActionSupport {
         
         return SUCCESS;
     }
+    
+    public void validate(){
+        if (personBean.getFirstName().length() == 0) {
+            addFieldError("personBean.firstName", "First name is required.");
+        }
 
-    @StrutsParameter(depth = 1)
+        if (personBean.getEmail().length() == 0) {
+            addFieldError("personBean.email", "Email is required.");
+        }
+
+        if (personBean.getAge() < 18) {
+            addFieldError("personBean.age", "Age is required and must be 18 or older");
+        }
+    }
+
     public Person getPersonBean() {
         return personBean;
     }
